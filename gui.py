9808f79dda0915e661d54d25642a02e60b876783
CobbCoding1 <rircobb@gmail.com>
2020-12-16 18:45:19 -0500

    made the gui a lot better

--- gui.py
+++ gui.py
@@ -7,17 +7,32 @@ def getFile():
     global result
     filename = filedialog.askopenfilename()
     result = convert(filename)
+    result = str(result).replace('(', '')
+    result = result.replace(')', '')
+    result = result.replace("'", '')
+    addInfo(result)
     print(result)
 
 def mainGUI():
     global result
+    global parsedText
     root = Tk()
+    root.geometry('600x400')
+    root.configure(background='light gray')
 
-    imgButton = Button(command=getFile)
-    imgButton.place(relx=.5, rely=.5, relwidth=.1, relheight=.1)
+    parsedText = StringVar()
+    parsedInfo = Entry(textvariable=parsedText, font='Helvecta 25')
+    parsedInfo.place(relx=.1, rely=.5, relwidth=.8, relheight=.2)
 
-    submitButton = Button(command=root.destroy, text='Submit')
-    submitButton.place(relx=.9, rely=.5, relwidth=.1, relheight=.1)
+    imgButton = Button(command=getFile, text='Upload', bg='dark gray')
+    imgButton.place(relx=.375, rely=.2, relwidth=.25, relheight=.15)
+
+    submitButton = Button(command=root.destroy, text='Submit', bg='dark gray')
+    submitButton.place(relx=.4, rely=.8, relwidth=.2, relheight=.1)
 
     root.mainloop()
     return(result)
+
+def addInfo(result):
+    global parsedText
+    parsedText.set(str(result))
