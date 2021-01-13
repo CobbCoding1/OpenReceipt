247d6d53fc952c3b26dad0f7f3842d66932f00d8
CobbCoding1 <rircobb@gmail.com>
2021-01-13 16:14:32 -0500

    Added comments

--- gui.py
+++ gui.py
@@ -5,32 +5,42 @@ import threading
 
 def getFile():
     global result
+    # Ask for filename from user
     filename = filedialog.askopenfilename(multiple=True)
     print(filename)
+    # Parse results to remove bloat
     result = convert(filename)
     result = str(result).replace("'", '')
     result = result.replace('[', '')
     result = result.replace(']', '')
+
+    # Set the display to the result
     addInfo(result)
     print(result)
 
 def mainGUI():
     global result
     global parsedText
+
+    # Create the main GUI
     root = Tk()
     root.geometry('600x400')
     root.configure(background='light gray')
 
+    # Add entry for result
     parsedText = StringVar()
     parsedInfo = Entry(textvariable=parsedText, font='Helvecta 25')
     parsedInfo.place(relx=.1, rely=.5, relwidth=.8, relheight=.2)
 
+    # Add button for getting the image from user
     imgButton = Button(command=getFile, text='Upload', bg='dark gray')
     imgButton.place(relx=.375, rely=.2, relwidth=.25, relheight=.15)
 
+    # Add submit button to send results to excel file
     submitButton = Button(command=root.destroy, text='Submit', bg='dark gray')
     submitButton.place(relx=.4, rely=.8, relwidth=.2, relheight=.1)
 
+    # Mainloop
     root.mainloop()
     return(result)
 
--- parse.py
+++ parse.py
@@ -7,35 +7,47 @@ import cv2
 
 def convert(imgs):
     results = []
+
+    # Loop through images in user input
     for img in imgs:
+        # Open names file to get all store names
         file = open('data/names.txt', 'r')
         names = []
         name = ''
 
+        # Loop through lines in name file
         for line in file:
             names.append(line)
 
+        # Parse names data
         for i in range(len(names)):
             names[i] = names[i].replace('\n', '')
 
+        # Read text on users image
         text = pytesseract.image_to_string(Image.open(img))
         print(text)
+
+        # Extract date from image
         date = re.search('[0-9][0-9]/[0-9][0-9]/[0-9]+', text)
         if(date == None):
             date = re.search('[0-9][0-9]\[0-9][0-9][0-9]\[0-9][0-9]+', text)
         if(date == None):
             date = 'N'
 
+        # Extract price from image
         price = re.findall('[0-9]+\.[0-9]+', text)
         if(price == None or price == []):
             price = 'N'
 
+        # Extract store name from image
         for i in range(len(names)):
             if names[i] in text:
                 name = names[i]
                 break
             else:
                 name = 'Not Found'
+
+        # Add parsed information to results variable
         results.append((date[0], price[-1], name))
         print(results)
     return(results)
